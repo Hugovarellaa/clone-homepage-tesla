cc523ba1e2e969a966884f2d7c9417411ade5bb3
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-10-05 09:31:14 -0300

    Criando os botões do react-modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from "react-modal";
-import { Container } from "./styles";
+import { Container, ContentButtons } from "./styles";
 import closeImg from "../../images/assets/close.svg";
+import incomeImg from "../../images/assets/income.svg";
+import outcomeImg from "../../images/assets/outcome.svg";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -19,12 +21,26 @@ export function NewTransactionModal({
       className="react-modal-content"
     >
       <button type="button" onClick={onRequestClose}>
-        <img src={closeImg} alt="Close button" className="react-modal-close-button"/>
+        <img
+          src={closeImg}
+          alt="Close button"
+          className="react-modal-close-button"
+        />
       </button>
       <Container>
         <h2>Cadastra transação</h2>
         <input placeholder="Títutlo" />
         <input type="number" placeholder="Valor" />
+        <ContentButtons>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Saida" />
+            <span>Saída</span>
+          </button>
+        </ContentButtons>
 
         <input placeholder="Categoria" />
         <button type="submit">Cadastrar</button>
